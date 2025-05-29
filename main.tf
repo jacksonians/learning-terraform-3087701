fd1dcee9c4d452ed303f67f6e3d6dfce7ff23a52
Jackson <147094861+jacksonians@users.noreply.github.com>
2025-05-29 14:15:14 -0700

    fixed errors 2

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port   = 80
   protocol  = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
@@ -51,7 +51,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port   = 443
   protocol  = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
@@ -60,5 +60,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   to_port   = 0
   protocol  = -1
   cidr_blocks = ["0.0.0.0/0"]
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
