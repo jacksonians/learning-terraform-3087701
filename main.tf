fa1f8697e7d5f7ae248aae87137fdef16f57a1b9
Jackson <147094861+jacksonians@users.noreply.github.com>
2025-05-30 15:40:49 -0700

    fixing module

--- main.tf
+++ main.tf
@@ -45,22 +45,25 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  name = "blog-alb"
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
-  }
+  ]
 
   http_tcp_listeners = [
     {
